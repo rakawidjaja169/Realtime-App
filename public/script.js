59b8dcc91f5c61a4ac869128886988a0c10b373d
rakawidjaja169 <54425993+rakawidjaja169@users.noreply.github.com>
2021-11-26 20:19:08 +0700

    Update

---
+++ public/script.js
@@ -0,0 +1,207 @@
+const socket = io('http://localhost:3000')
+const messageContainer = document.getElementById('message-container')
+const roomContainer = document.getElementById('room-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+
+const BG_COLOUR = '#00FF00';
+const BG_COLOUR2 = '#FF0000';
+
+var canvas;
+var ctx;
+
+var timer = 30;
+
+var player;
+
+var leftPaddleY = 190;
+var leftPaddleX = 200;
+const PADDLE_HEIGHT = 50;
+const PADDLE_WIDTH = 50;
+
+var player1Score = 0;
+var player2Score = 0;
+const WINNING_SCORE = 11;
+
+var pos = "true";
+
+var query = { msg: "Siapa anda?"};
+
+startButton.addEventListener('click', () => {
+  socket.emit('startIsClicked', roomName, timer, query);
+});
+
+score1.addEventListener('click', () => {
+  socket.emit('giveScore', roomName, player1Score);
+});
+
+nextRound.addEventListener('click', () => {
+  socket.emit('nextRound', roomName, timer);
+});
+
+socket.on('getRoomId', roomId => {
+  showRoomId(roomId);
+}) 
+
+socket.on('startIsClicked', () => {
+  init();
+});
+
+socket.on('start-round', () => {
+  startRound();
+})
+
+socket.on('keyIsPressed', (data) => {
+  moveObject(data.leftPaddleX, data.leftPaddleY);
+});
+
+socket.on('countdown', timeleft => {
+  countdown(timeleft);
+});
+
+socket.on('giveScore', score => {
+  player1Score = score;
+});
+
+socket.on('end-round', () => {
+  endRound();
+});
+
+socket.on('question', question => {
+  showQuestion(question);
+})
+
+function showRoomId(room) {
+  document.getElementById("roomId").innerHTML = room;
+}
+
+function showQuestion(question) {
+  document.getElementById("question").innerHTML = question;
+}
+
+function startRound() {
+  canvas.style.display = "block";
+  leaderCanvas.style.display = "none";
+  header.style.display = "block";
+}
+
+function endRound() {
+  canvas.style.display = "none";
+  leaderCanvas.style.display = "block";
+  header.style.display = "none";
+  document.getElementById("leaderboard").innerHTML = player + " " + player1Score;
+}
+
+function moveObject(offX, offY) {
+  leftPaddleX = offX;
+  leftPaddleY = offY;
+}
+
+function countdown(timeleft) {
+  if(timeleft >= 0){
+    document.getElementById("timer").innerHTML = timeleft ;
+  }
+}
+
+function init() {
+  startButton.style.display = "none";
+  leaderCanvas.style.display = "none";
+
+  canvas = document.getElementById('canvas');
+	ctx = canvas.getContext('2d');
+	
+	var framePerSecond = 60;
+	setInterval(main, 1000/framePerSecond);
+	
+	document.addEventListener('keydown',
+    function onkeydown(e) {
+      if(e.keyCode==39) {pos = "false"} //right arrow
+      else if(e.keyCode==37) {pos = "true"} //left arrow
+      else if(e.keyCode==38) {leftPaddleY-=10;} //up arrow
+      else if(e.keyCode==40) {leftPaddleY+=10;} //down arrow
+
+      if(pos == "true") {
+        leftPaddleX = 200;
+      } else {
+        leftPaddleX = 700;
+      }
+
+      socket.emit('keyIsPressed', roomName, {
+        leftPaddleX,
+        leftPaddleY
+      });
+    }
+  );
+}
+
+function main() {
+	drawEverything();
+}
+
+function drawEverything() {
+
+  //True Canvas
+	colorRect(0, 0, canvas.width/2, canvas.height, BG_COLOUR);
+
+  //False Canvas
+	colorRect(canvas.width/2, 0, canvas.width/2, canvas.height, BG_COLOUR2);
+
+	//Left Paddle
+	colorRect(leftPaddleX, leftPaddleY, PADDLE_WIDTH, PADDLE_HEIGHT, 'black');
+	
+	//Line
+	colorRect(canvas.width/2, 0, 6, 800, 'white');
+	
+	//Score
+	ctx.fillText(player1Score, 300, 100);
+	ctx.fillText(player2Score, (canvas.width/2)+100, 100);
+
+}
+
+function colorRect(x, y, width, height, drawColor) {
+	ctx.fillStyle = drawColor;
+	ctx.fillRect(x, y, width, height);
+}
+
+if (messageForm != null) {
+  const name = prompt('What is your name?')
+  player = name;
+  appendMessage('You joined')
+  socket.emit('new-user', roomName, name)
+
+  messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    appendMessage(`You: ${message}`)
+    socket.emit('send-chat-message', roomName, message)
+    messageInput.value = ''
+  })
+}
+
+socket.on('room-created', room => {
+  const roomElement = document.createElement('div')
+  roomElement.innerText = room
+  const roomLink = document.createElement('a')
+  roomLink.href = `/${room}`
+  roomLink.innerText = 'join'
+  roomContainer.append(roomElement)
+  roomContainer.append(roomLink)
+})
+
+socket.on('chat-message', data => {
+  appendMessage(`${data.name}: ${data.message}`)
+})
+
+socket.on('user-connected', name => {
+  appendMessage(`${name} connected`)
+})
+
+socket.on('user-disconnected', name => {
+  appendMessage(`${name} disconnected`)
+})
+
+function appendMessage(message) {
+  const messageElement = document.createElement('div')
+  messageElement.innerText = message
+  messageContainer.append(messageElement)
+}
---
+++ server.js
@@ -0,0 +1,124 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+const mongoose = require('mongoose');
+const Msg = require('./models/messages');
+const mongoDB = 'mongodb+srv://deved:bolehlah@cluster0.ff26a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+  console.log('connected')
+}).catch(err => console.log(err))
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+const rooms = { }
+
+app.get('/', (req, res) => {
+  res.render('index', { rooms: rooms })
+})
+
+app.post('/room', (req, res) => {
+  var roomId = makeid(5);
+  if (rooms[roomId] != null) {
+    return res.redirect('/')
+  }
+  rooms[roomId] = { users: {} }
+  res.redirect(roomId)
+  // Send message that new room was created
+  io.emit('room-created', roomId)
+})
+
+app.get('/:room', (req, res) => {
+  if (rooms[req.params.room] == null) {
+    return res.redirect('/')
+  }
+  res.render('room', { roomName: req.params.room })
+})
+
+server.listen(3000)
+
+io.on('connection', socket => {
+
+  socket.on('startIsClicked', (room, timer, query) => {
+    countdown(room, timer);
+    question(room, query);
+    io.to(room).emit('getRoomId', room)
+    io.to(room).emit('startIsClicked');
+  })
+
+  socket.on('nextRound', (room, timer) => {
+    countdown(room, timer);
+    io.to(room).emit('start-round');
+  }) 
+
+  socket.on('keyIsPressed', (room, data) => {
+    socket.to(room).broadcast.emit('keyIsPressed', data);
+  });
+
+  socket.on('giveScore', (room, score) => {
+    score += 1;
+    io.to(room).emit('giveScore', score);
+  });
+
+  socket.on('new-user', (room, name) => {
+    socket.join(room)
+    rooms[room].users[socket.id] = name
+    socket.to(room).broadcast.emit('user-connected', name)
+  })
+
+  socket.on('send-chat-message', (room, msg) => {
+    const message = new Msg({ msg });
+    message.save().then(() => {
+      socket.to(room).broadcast.emit('chat-message', { message: msg, name: rooms[room].users[socket.id] })
+    })
+  })
+
+  socket.on('disconnect', () => {
+    getUserRooms(socket).forEach(room => {
+      socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
+      delete rooms[room].users[socket.id]
+    })
+  })
+
+})
+
+function getUserRooms(socket) {
+  return Object.entries(rooms).reduce((names, [name, room]) => {
+    if (room.users[socket.id] != null) names.push(name)
+    return names
+  }, [])
+}
+
+function question(room, query) {
+  Msg.find(query).then(result => {
+    io.to(room).emit('question', result[0].msg)
+  })
+}
+
+function countdown(room, timer) {
+  var active = true;
+  var timeleft = timer;
+  var downloadTimer = setInterval(function(){
+    io.to(room).emit ('countdown', timeleft);
+    timeleft -= 1;
+    if (active == true && timeleft < 0) {
+      active = false
+      io.to(room).emit ('end-round');
+    }
+  }, 1000);
+}
+
+function makeid(length) {
+  var code           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+     code += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return code;
+}
